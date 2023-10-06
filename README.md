e16496e1f39886c59a2d5c88759415069418c3e5
KnotSarvajit <sarvajitvisagan@gmail.com>
2023-10-06 19:25:54 +0530

    Added login verification in services page and fixed nav css

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# React + Vite
+https://prod.liveshare.vsengsaas.visualstudio.com/join?4435BB7D2BE3078D66A8B9D932EC39ED2A18# React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/Services.jsx
+++ src/Services.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import Board from './components/board';
 import './components/style.css';
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 function Services() {
-  return (
-    <div className="Services" id='main'>
-        <Board></Board>
+  const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
+  console.log("Auth",isAuthenticated)
+
+  if(isLoading) return <p>Loading....</p>
+
+    if (!isAuthenticated) {
+      return loginWithRedirect() 
+      
+    }
+  console.log("Auth done",isAuthenticated)
+    
+    return (
+      <div  id='main'>
+        <Board/>
     </div>
   );
 }
